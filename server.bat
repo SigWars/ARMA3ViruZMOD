da77429d3762c612a96cb58cabe8787dea817250
Mateuus <mateuus27@outlook.com>
2016-08-31 05:50:59 -0300

    Update Hive configurações pra deixa online o server

--- server.bat
+++ server.bat
@@ -1,4 +1,4 @@
-arma3server.exe  -port=2302 "-config=C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Server\cfgviruz\config.cfg" "-cfg=C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Server\cfgviruz\basic.cfg" "-profiles=C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Server\cfgviruz" -name=cfgviruz -filePatching -autoinit -loadmissiontomemory "-mod=@hive;@ViruZ"
+arma3server.exe  -port=2302 "-config=C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Server\cfgviruz\config.cfg" "-cfg=C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Server\cfgviruz\basic.cfg" "-profiles=C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Server\cfgviruz" -name=cfgviruz -filePatching -autoinit -loadmissiontomemory "-mod=@hive;@CBA_A3;@ViruZ"
 
 echo ARMA 3 Server has started.
 
