efe704cda69758c06fde9fe2451e529e389e269e
Mateuus <mateuus27@outlook.com>
2016-09-30 08:23:50 -0300

    Retornando Trabalhos

--- server.bat
+++ server.bat
@@ -1,4 +1,4 @@
-arma3server.exe  -port=2302 "-config=C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Server\cfgviruz\config.cfg" "-cfg=C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Server\cfgviruz\basic.cfg" "-profiles=C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Server\cfgviruz" -name=cfgviruz -filePatching  -loadmissiontomemory "-mod=@hive;@CBA_A3;@infiSTAR_A3;@ViruZ"
+arma3server.exe  -port=2302 "-config=C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Server\cfgviruz\config.cfg" "-cfg=C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Server\cfgviruz\basic.cfg" "-profiles=C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Server\cfgviruz" -name=cfgviruz -filePatching "-mod=@hive;@CBA_A3;@infiSTAR_A3;@ViruZ;@CUP_Terrains_Core-1.2.0;@CUP_Terrains_Maps-1.2.0;@CUP_Weapons;" -autoinit -loadmissiontomemory
 
 echo ARMA 3 Server has started.
 
